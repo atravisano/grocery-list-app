6e83e905b7f67ca369f50b9c6a483a1bd77e1e84
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2022-03-19 13:00:09 -0400

    Create UI shell

--- client/grocery-list-app/src/app/app.module.ts
+++ client/grocery-list-app/src/app/app.module.ts
@@ -3,14 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GroceryFormComponent } from './grocery-list/components/grocery-form/grocery-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './grocery-list/containers/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './shared/components/navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { GroceryItemComponent } from './grocery-list/components/grocery-item/grocery-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryFormComponent,
+    DashboardComponent,
+    NavigationComponent,
+    GroceryItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
