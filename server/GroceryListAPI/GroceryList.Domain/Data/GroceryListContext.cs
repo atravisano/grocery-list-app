5fd9f6d71d5b0ae391b8e639ff876d712b528a3a
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2022-03-20 15:57:22 -0400

    Add ef core to domain project

---
+++ server/GroceryListAPI/GroceryList.Domain/Data/GroceryListContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryList.Domain.Data
+{
+    public class GroceryListContext : DbContext
+    {
+        public DbSet<GroceryItem>? GroceryItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GroceryList;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
--- server/GroceryListAPI/GroceryList.Domain/Models/GroceryItem.cs
+++ server/GroceryListAPI/GroceryList.Domain/Models/GroceryItem.cs
@@ -4,6 +4,9 @@ namespace GroceryList.Domain
 {
     public class GroceryItem
     {
+        [Key]
+        public int Id { get; set; }
+
         [Required]
         public string? Name { get; set; }
     }
