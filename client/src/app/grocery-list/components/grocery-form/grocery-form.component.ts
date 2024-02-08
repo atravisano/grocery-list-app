467952b61a37c767627a0c365b82d2a46211d0c1
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-02-08 16:17:27 -0500

    Update form onSubmit to use finalize and takeUntil

--- client/src/app/grocery-list/components/grocery-form/grocery-form.component.ts
+++ client/src/app/grocery-list/components/grocery-form/grocery-form.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { catchError, of, take, tap } from 'rxjs';
+import { Subject, catchError, finalize, of, takeUntil, tap } from 'rxjs';
 import { GroceryListService } from '../../services/grocery-list/grocery-list.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { GroceryListService } from '../../services/grocery-list/grocery-list.ser
   templateUrl: './grocery-form.component.html',
   styleUrls: ['./grocery-form.component.scss']
 })
-export class GroceryFormComponent {
+export class GroceryFormComponent implements OnDestroy {
   public groceryForm = this.fb.group({
     // Note: `Validator.required` would typically be added here.
     // It was not added because of the input turning red after form submission by default in Angular Material.
@@ -17,10 +17,16 @@ export class GroceryFormComponent {
     item: [null, [Validators.maxLength(255)]],
   });
   public isLoading = false;
+  private readonly destroy$ = new Subject<void>();
 
   constructor(private fb: UntypedFormBuilder,
               private groceryListService: GroceryListService,
               private snackBarService: MatSnackBar) {}
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   /**
    * Add a grocery item to the list.
@@ -29,7 +35,7 @@ export class GroceryFormComponent {
     this.isLoading = true;
     this.groceryListService.create(this.groceryForm.controls['item'].value)
       .pipe(
-        take(1),
+        takeUntil(this.destroy$),
         tap(() => {
           this.groceryForm.reset();
           this.groceryListService.refreshItems();
@@ -39,7 +45,8 @@ export class GroceryFormComponent {
           this.snackBarService.open('Item could not be added.', 'Dismiss')
           this.isLoading = false;
           return of(undefined);
-        })
-      ).subscribe(() => this.isLoading = false);
+        }),
+        finalize(() => this.isLoading = false)
+      ).subscribe();
   }
 }
